a6e59ddbad184db1d06900a514c75522c8ded4e5
William Andres Gutierrez Robayo <141888818+WilliamGuti@users.noreply.github.com>
2025-05-31 19:35:24 -0400

    Update capaOcultaEntrenamiento.py

--- capaOcultaEntrenamiento.py
+++ capaOcultaEntrenamiento.py
@@ -3,40 +3,40 @@ import numpy as np
 import os
 from time import time
 
-#traer la carpta donde esta la informacion
-dataRuta = 'C:/Users/w/OneDrive/Desktop/proyectoReconocimientoDeImagenes/reconocimientoFacial1CapturaDatos/Data'
-listaData = os.listdir(dataRuta)
+# load the folder where the face data is stored
+data_path = 'path where you'll charge data capture'
+data_list = os.listdir(data_path)
 
-# añadir etiquetas para diferenciar
-ids = []
-rostrosData = []
-id = 0
+# lists to store image data and corresponding labels
+labels = []
+face_data = []
+label_id = 0
 
-tiempoInicial = time()
+start_time = time()
 
-for fila in listaData:
-    rutaCompleta =  dataRuta + '/' + fila
-    print('Iniciando Lectura...')
-    for archivo in os.listdir(rutaCompleta):
-        print('imagenes: ', fila + '/' + archivo )
-        ids.append(id)
-        rostrosData.append(cv2.imread(rutaCompleta + '/' + archivo, 0)) # se puede escalar a grises con el 0 para no hacer lo de bar binry
-        
-    id += 1
-
-    tiempoFinalLectura = time()
-    tiempoTotalLectura = tiempoFinalLectura - tiempoInicial
-    print('Tiempo Total De Lectura: ' , tiempoTotalLectura) 
+for folder_name in data_list:
+    full_path = data_path + '/' + folder_name
+    print('Starting to read images...')
     
-
-#entrenamiento de reconocimiento facial
-entrenamientoModelo1 = cv2.face.EigenFaceRecognizer_create()
-print("Iniciando Entrenamiento ... Espere")
-entrenamientoModelo1.train(rostrosData, np.array(ids))
-tiempoFinalLectura = time()
-tiempoTotalLectura = tiempoFinalLectura - tiempoInicial
-print("tiepo de entrenamiento total: " , tiempoTotalLectura)
-entrenamientoModelo1.write('EntrenamientoEigenFaceRecognizer.xml')
-print('Entrenamiento Completado')
-
+    for file in os.listdir(full_path):
+        print('Image:', folder_name + '/' + file)
+        labels.append(label_id)
+        # Load image in grayscale
+        face_data.append(cv2.imread(full_path + '/' + file, 0)) 
         
+    label_id += 1
+
+    read_end_time = time()
+    read_duration = read_end_time - start_time
+    print('Total Reading Time:', read_duration)
+
+# training the face recognition model
+face_recognizer = cv2.face.EigenFaceRecognizer_create()
+print("Starting training... Please wait.")
+face_recognizer.train(face_data, np.array(labels))
+training_end_time = time()
+training_duration = training_end_time - start_time
+print("Total Training Time:", training_duration)
+
+face_recognizer.write('EigenFaceRecognizerTraining.xml')
+print('Training Completed')
