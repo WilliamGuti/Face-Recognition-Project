772ec5413de6384e64f7ec918a5deabc13f3c974
William Andres Gutierrez Robayo <141888818+WilliamGuti@users.noreply.github.com>
2025-05-31 19:33:04 -0400

    Update capaDeEntradaCapturaDatos.py

--- capaDeEntradaCapturaDatos.py
+++ capaDeEntradaCapturaDatos.py
@@ -1,59 +1,61 @@
 import cv2 as cv
 import numpy as np
-import os #sirve para crear archivos con codigo python
+import os  # used to create directories with Python code
 import imutils
 
-modelo = 'FotosWilliam' #nombre de como se va a llamar la carpeta
-ruta1 = 'C:/Users/w/OneDrive/Desktop/proyectoReconocimientoDeImagenes/reconocimientoFacial1CapturaDatos'
-rutaCompleta = ruta1 + '/' + modelo
+model_name = 'WilliamPhotos'  # name of the folder to be created
+base_path = 'C:/Users/w/OneDrive/Desktop/proyectoReconocimientoDeImagenes/reconocimientoFacial1CapturaDatos'
+full_path = base_path + '/' + model_name
 
-#si no existe la ruta, que la cree
-if not os.path.exists(rutaCompleta):
-    os.makedirs(rutaCompleta)
+# create the directory if it doesn't exist
+if not os.path.exists(full_path):
+    os.makedirs(full_path)
 
+# pre-trained classifier for frontal face detection
+face_cascade = cv.CascadeClassifier(
+    'C:/Users/w/OneDrive/Desktop/proyectoReconocimientoDeImagenes/entrenamientosOpencvRuidos/opencv-master/data/haarcascades/haarcascade_frontalface_default.xml'
+)
 
+# activate webcam
+camera = cv.VideoCapture(0)
 
-# ruidos ya prestablecidos cara frontal
-ruidos = cv.CascadeClassifier('C:/Users/w/OneDrive/Desktop/proyectoReconocimientoDeImagenes/entrenamientosOpencvRuidos/opencv-master/data/haarcascades/haarcascade_frontalface_default.xml')
-
-#activar camara
-camara = cv.VideoCapture(0)
-
-# id de cada imagen
-id = 0
+# image ID
+img_id = 0
 
 while True:
-    respuesta, captura = camara.read()
-    if respuesta == False:
-        print("No se puede capturar la camara")
-        break
-    # bajarle la resolucion en caso de que tenga muy alta calidad
-    captura = imutils.resize(captura, width = 640)
-    #hacemos escala de grises
-    grises = cv.cvtColor(captura, cv.COLOR_BGR2GRAY)
-    # deteccion del rostro con los ruidos
-    caras = ruidos.detectMultiScale(grises, 1.3, 3)
-
-    # id de la captura de cada imagen
-    idCaptura = captura.copy()
-
-    #hacemos el rectangulo 
-    for (x,y,e1,e2) in caras:
-        cv.rectangle(captura,(x,y),(x+e1,y+e2),(0,0,255),2)
-        #sacar fragmentos de nuestro rostro y almarcenarlos en la carpeta
-        rostroCapturado = idCaptura[y:y+e2 , x:x+e1]
-        # modificar el tamaño de las fotos
-        rostroCapturado = cv.resize(rostroCapturado,(160,160), interpolation=cv.INTER_CUBIC)
-        # modificar el nombre de cada fotos
-        cv.imwrite(rutaCompleta + '/imagen_{}.jpg'.format(id), rostroCapturado)
-        # aumentar el id
-        id += 1
-
-
-    cv.imshow("Resultado Rostro", captura)
-    
-    if id == 351:
+    success, frame = camera.read()
+    if not success:
+        print("Cannot access the camera")
         break
 
-camara.release()
+    # resize frame in case of high resolution
+    frame = imutils.resize(frame, width=640)
+    # convert to grayscale
+    gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+    # detect faces in the frame
+    faces = face_cascade.detectMultiScale(gray, 1.3, 3)
+
+    # make a copy of the original frame for cropping
+    frame_copy = frame.copy()
+
+    # draw rectangles around detected faces and save them
+    for (x, y, w, h) in faces:
+        cv.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 2)
+        # crop the face from the frame
+        captured_face = frame_copy[y:y + h, x:x + w]
+        # resize the cropped face
+        captured_face = cv.resize(captured_face, (160, 160), interpolation=cv.INTER_CUBIC)
+        # save the image to the folder
+        cv.imwrite(full_path + '/image_{}.jpg'.format(img_id), captured_face)
+        # increment the image ID
+        img_id += 1
+
+    # show the frame with rectangles
+    cv.imshow("Face Detection Result", frame)
+
+    # stop when 351 images have been saved
+    if img_id == 351:
+        break
+
+camera.release()
 cv.destroyAllWindows()
