c25fc4f30915401a2b3fbe98138920aa1d48ed9a
William Andres Gutierrez Robayo <141888818+WilliamGuti@users.noreply.github.com>
2025-05-31 19:36:42 -0400

    Update capaSalidaRecFacial.py

--- capaSalidaRecFacial.py
+++ capaSalidaRecFacial.py
@@ -1,36 +1,43 @@
 import cv2 as cv
 import os
 
-#traer la carpta donde esta la informacion
-dataRuta = 'C:/Users/w/OneDrive/Desktop/proyectoReconocimientoDeImagenes/reconocimientoFacial1CapturaDatos/Data'
-listaData = os.listdir(dataRuta)
-#entrenamiento de reconocimiento facial
-entrenamientoModelo1 = cv.face.EigenFaceRecognizer_create()
-entrenamientoModelo1.read('EntrenamientoEigenFaceRecognizer.xml')
-ruidos = cv.CascadeClassifier('C:/Users/w/OneDrive/Desktop/proyectoReconocimientoDeImagenes/entrenamientosOpencvRuidos/opencv-master/data/haarcascades/haarcascade_frontalface_default.xml')
-camara = cv.VideoCapture(0)
+# Load the folder where the face data is stored
+data_path = 'path folder where the face data is stored'
+data_list = os.listdir(data_path)
+
+# Load the trained face recognition model
+face_recognizer = cv.face.EigenFaceRecognizer_create()
+face_recognizer.read('EigenFaceRecognizerTraining.xml')
+
+# Load face detection classifier
+face_cascade = cv.CascadeClassifier('load face detection classifies/OpencvRuidos/opencv-master/data/haarcascades/haarcascade_frontalface_default.xml')
+
+# Activate the camera
+camera = cv.VideoCapture(0)
 
 while True:
-    _,captura = camara.read()
-    grises = cv.cvtColor(captura, cv.COLOR_BGR2GRAY)
-    idCaptura = grises.copy()
-    cara = ruidos.detectMultiScale(grises, 1.3, 5)
-    # enmarcar la imagen
-    for(x,y,e1,e2) in cara:
-        rostrocapturado=idCaptura[y:y+e2,x:x+e1]
-        rostrocapturado=cv.resize(rostrocapturado, (160,160),interpolation=cv.INTER_CUBIC)
-        resultado=entrenamientoModelo1.predict(rostrocapturado)
-        cv.putText(captura, '{}'.format(resultado), (x,y-5), 1,1.3,(0,255,0),1,cv.LINE_AA)
-        if resultado[1]<8000:
-            cv.putText(captura, '{}'.format(listaData[resultado[0]]), (x,y-20), 2,1.1,(0,255,0),1,cv.LINE_AA)
-            cv.rectangle(captura, (x,y), (x+e1,y+e2), (255,0,0),2)
-        else:
-            cv.putText(captura,"No encontrado", (x,y-20), 2,0.7,(0,255,0),1,cv.LINE_AA)
-            cv.rectangle(captura, (x,y), (x+e1,y+e2), (255,0,0),2)
+    _, frame = camera.read()
+    gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+    gray_copy = gray.copy()
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
 
-       
-    cv.imshow("Resultados", captura)
-    if cv.waitKey(1)==ord('s'):
+    # Draw rectangles around detected faces
+    for (x, y, w, h) in faces:
+        captured_face = gray_copy[y:y+h, x:x+w]
+        captured_face = cv.resize(captured_face, (160, 160), interpolation=cv.INTER_CUBIC)
+        result = face_recognizer.predict(captured_face)
+        cv.putText(frame, '{}'.format(result), (x, y - 5), 1, 1.3, (0, 255, 0), 1, cv.LINE_AA)
+
+        if result[1] < 8000:
+            cv.putText(frame, '{}'.format(data_list[result[0]]), (x, y - 20), 2, 1.1, (0, 255, 0), 1, cv.LINE_AA)
+            cv.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
+        else:
+            cv.putText(frame, "Not recognized", (x, y - 20), 2, 0.7, (0, 255, 0), 1, cv.LINE_AA)
+            cv.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
+
+    cv.imshow("Results", frame)
+    if cv.waitKey(1) == ord('s'):
         break
-camara.release()
+
+camera.release()
 cv.destroyAllWindows()
